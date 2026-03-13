8ba8b26cabe2cac9d45da9a6e5702ea10139aa59
rentech12 <samyvladimir12@gmail.com>
2026-03-13 18:27:07 -0400

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '⌬ 𝗥𝗢𝗦𝗔 𝗔𝗜 ⌬/n/n/n/n/n/n/n/n𝐌ꝛ 𝐑𝐄𝐍 𝚨𝚻𝚨𝚳𝚵 (𝚃𝙷𝙴 𝙾𝙽𝙻𝚈 𝙾𝙽𝙴 𝙰𝚃𝙰𝙼𝙴',
+  author: '𝐌ꝛ 𝐑𝐄𝐍 𝚨𝚻𝚨𝚳𝚵 (𝚃𝙷𝙴 𝙾𝙽𝙻𝚈 𝙾𝙽𝙴 𝙰𝚃𝙰𝙼𝙴',
+  botName: "⌬ 𝗥𝗢𝗦𝗔 𝗔𝗜 ⌬",
+  botOwner: '𝐌ꝛ 𝐑𝐄𝐍 𝚨𝚻𝚨𝚳𝚵 (𝚃𝙷𝙴 𝙾𝙽𝙻𝚈 𝙾𝙽𝙴 𝙰𝚃𝙰𝙼𝙴)', // Your name
+  ownerNumber: '50934264629', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.4.0",
+  updateZipUrl: "https://github.com/queen-ai-bot-md/queen_ai_md-main.zip",
+};
+
+module.exports = settings;
